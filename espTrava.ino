ee1bd2993a39f49a095559df02f0277ce46b1643
id-alina-bugakova <id.alina.bugakova@yandex.ru>
2022-04-25 12:22:12 +0300

    Added "s in MAC in JSON, fixed syntax, comments, some tabulations.

--- espTrava.ino
+++ espTrava.ino
@@ -11,13 +11,13 @@
 //OneWire oneWire(ONE_WIRE_BUS);
 //DallasTemperature sensors(&oneWire);
 
-const char *ssid = "RaspiNetwork"; // Имя вайфай точки доступа
-const char *pass = "raspberry"; // Пароль от точки доступа
+const char *ssid = "RaspiNetwork";          // Имя вайфай точки доступа
+const char *pass = "raspberry";             // Пароль от точки доступа
 
-const char *mqtt_server = "192.168.4.1"; // Имя сервера MQTT
-const int mqtt_port = 1883; // Порт для подключения к серверу MQTT
-const char *mqtt_user = ""; // Логи от сервер
-const char *mqtt_pass = ""; // Пароль от сервера
+const char *mqtt_server = "192.168.4.1";    // Имя сервера MQTT
+const int mqtt_port = 1883;                 // Порт для подключения к серверу MQTT
+const char *mqtt_user = "";                 // Логи от сервер
+const char *mqtt_pass = "";                 // Пароль от сервера
 
 #define BUFFER_SIZE 100
 
@@ -31,64 +31,58 @@ float temp = 0;
 
 class cell {
     public:      
-      cell(word waterS, uint8_t dhtPin, byte pump, byte lightPin, int lightTimeUp, int lightTimeDown,  int hbDelay) {
-        this->_id = id;
-        this->_waterS = waterS;
-        this->_dhtPin = dhtPin;
-        this->_pump = pump;
-        this->_hbDelay = hbDelay;
-        this->_dht = DHT(this->_dhtPin, DHT11);
-        _lightPin = lightPin;
-        _lightTimeUp = lightTimeUp;
-        _lightTimeDown = lightTimeDown;
-        pinMode(this->_waterS, INPUT);
-        pinMode(this->_dhtPin, INPUT);
-        pinMode(this->_pump, OUTPUT);
-        pinmode(_lightPin, OUTPUT);
-        this->_dht.begin();
-      }
+        cell(word waterS, uint8_t dhtPin, byte pump, byte lightPin, int lightTimeUp, int lightTimeDown,  int hbDelay) {
+            this->_waterS = waterS;
+            this->_dhtPin = dhtPin;
+            this->_pump = pump;
+            this->_hbDelay = hbDelay;
+            this->_dht = DHT(this->_dhtPin, DHT11);
+            this->_lightPin = lightPin;
+            this->_lightTimeUp = lightTimeUp;
+            this->_lightTimeDown = lightTimeDown;
+            pinMode(this->_waterS, INPUT);
+            pinMode(this->_dhtPin, INPUT);
+            pinMode(this->_pump, OUTPUT);
+            pinMode(this->_lightPin, OUTPUT);
+            this->_dht.begin();
+        }
 
-      byte getWater(){
-        return digitalRead(this->_waterS);
-      }
-      int getHum()
-      {
-        int h = this->_dht.readHumidity();
-        if(isnan(h))
-        {
-          Serial.println("Failed to address DHT11 to get humidity.");
-          return -1;
+        byte getWater(){
+            return digitalRead(this->_waterS);
         }
-        return h;
-      }
+      
+        int getHum() {
+            int h = this->_dht.readHumidity();
+            if(isnan(h)) {
+                Serial.println("Failed to address DHT11 to get humidity.");
+                return -1;
+            }
+            return h;
+        }
 
-      int getTemp()
-      {
-        int t = this->_dht.readTemperature();
-        if(isnan(t))
-        {
-          Serial.println("Failed to address DHT11 to get temperature.");
-          return -1;
+        int getTemp() {
+            int t = this->_dht.readTemperature();
+            if(isnan(t)) {
+              Serial.println("Failed to address DHT11 to get temperature.");
+              return -1;
+            }
+            return t;
         }
-        return t;
-      }
 
-      void checkDHT()
-      {
-        float h, t;
-        h = this->getHum();
-        t = this->getTemp();
-        if(h != -1.0)
+        void checkDHT()    // Not used in project; prints data from DHT into Serial.
         {
-          Serial.print("Humidity:    ");
-          Serial.println(h);
+            float h, t;
+            h = this->getHum();
+            t = this->getTemp();
+            if(h != -1.0) {
+              Serial.print("Humidity:    ");
+              Serial.println(h);
+            }
+            if(t != -1.0) {
+              Serial.print("Temperature: ");
+              Serial.println(t);
+            }
         }
-        if(t != -1.0)
-        {
-          Serial.print("Temperature: ");
-          Serial.println(t);
-        }
-      }
       
       void checkWater(){
         Serial.println(this->getWater());
@@ -101,20 +95,18 @@ class cell {
       }
 
       void TimersInit(){
-        //this->_sleep = sleep;
-        //this->_work = work;
         this->_lastMilHB = -100000;
         this->_lastMilLight = -100000;
       }
         
         void sendHeartbeat(bool now){
             if (now){
-                String json = "{\"ID\": " + String(WiFi.macAddress()) + ", \"Humidity\": " + String(this->getHum()) + ", \"Temperature\": " + String(this->getTemp()) + "}";
+                String json = "{\"ID\": \"" + String(WiFi.macAddress()) + "\", \"Humidity\": " + String(this->getHum()) + ", \"Temperature\": " + String(this->getTemp()) + "}";
                 client.publish("test/heartbeat", json);
             }
             else{
                 if ((millis() - this->_lastMilHB) >= this->_hbDelay){
-                    String json = "{\"ID\": " + String(WiFi.macAddress()) + ", \"Humidity\": " + String(this->getHum()) + ", \"Temperature\": " + String(this->getTemp()) + "}";
+                    String json = "{\"ID\": \"" + String(WiFi.macAddress()) + "\", \"Humidity\": " + String(this->getHum()) + ", \"Temperature\": " + String(this->getTemp()) + "}";
                     client.publish("test/heartbeat", json);
                     this->_lastMilHB = millis();
                 }
@@ -122,21 +114,21 @@ class cell {
         }
         
         void lightLoop(){
-            if (not(_lightState) & ((millis()-_lastMilLight)>=_lightTimeDown)){
-                digitalWrite(_lightPin, HIGH);
-                _lastMilLight = millis();
-                _lightState = 1;
+            if (!this->_lightState && (millis() - this->_lastMilLight) >= this->_lightTimeDown){
+                digitalWrite(this->_lightPin, HIGH);
+                this->_lastMilLight = millis();
+                this->_lightState = 1;
             }
-            else if (_lightState & ((millis()-_lastMilLight )>=_lightTimeDown)){
-                digitalWrite(_lightPin, LOW);
-                _lastMilLight = millis();
-                _lightState = 0;
+            else if (this->_lightState && (millis() - this->_lastMilLight) >= this->_lightTimeDown){
+                digitalWrite(this->_lightPin, LOW);
+                this->_lastMilLight = millis();
+                this->_lightState = 0;
             }
         }
 
         void updateLoops(){
-            lightLoop();
-            sendHeartbeat(false);
+            this->lightLoop();
+            this->sendHeartbeat(false);
         }
     private:
         int _lastMilHB;
@@ -156,24 +148,24 @@ class cell {
         int _hbDelay;
 };
 
+
 uint8_t dhtPin = 14; //D5
 cell cell1 = cell(14, dhtPin, 12, LED_BUILTIN, 5000, 2000, 5000);
 
-// Функция получения данных от сервера
 
-void callback(const MQTT::Publish& pub)
+void callback(const MQTT::Publish& pub)                      // Функция получения данных от сервера:
 {
-  Serial.print(pub.topic()); // выводим в сериал порт название топика
-  Serial.print(" => ");
+    Serial.print(pub.topic());                               //   выводим в сериал порт название топика
+    Serial.print(" => ");
 
-  Serial.println(String(pub.payload_string().toInt())); // выводим в сериал порт значение полученных данных
-  String payload = pub.payload_string();
+    Serial.println(String(pub.payload_string().toInt()));    //   выводим в сериал порт значение полученных данных
+    String payload = pub.payload_string();
 
-  if (String(pub.topic()) == "test/blink") // проверяем из нужного ли нам топика пришли данные
-  {
-    int stled = payload.toInt(); // преобразуем полученные данные в тип integer
-    digitalWrite(LED_BUILTIN,  abs(stled-1)); // включаем или выключаем светодиод в зависимоти от полученных значений данных
-  }
+    if (String(pub.topic()) == "test/blink")                 //   проверяем из нужного ли нам топика пришли данные
+    {
+        int stled = payload.toInt();                         //   преобразуем полученные данные в тип integer
+        digitalWrite(LED_BUILTIN,  abs(stled-1));            //   включаем или выключаем светодиод в зависимоти от полученных значений данных
+    }
 
     if (String(pub.topic()) == "test/heartbeat"){
         cell1.sendHeartbeat(true);
@@ -182,46 +174,48 @@ void callback(const MQTT::Publish& pub)
 
 
 void setup() {
+    //sensors.begin();
     cell1.TimersInit();
-  //sensors.begin();
-  Serial.begin(115200);
-  delay(10);
-  Serial.println();
-  Serial.println();
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    delay(10);
+    Serial.println();
+    Serial.println();
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // подключаемся к wi-fi
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Connecting to ");
-    Serial.print(ssid);
-    Serial.println("...");
-    WiFi.begin(ssid, pass);
+void loop() 
+{
+    // подключаемся к wi-fi
+    if (WiFi.status() != WL_CONNECTED) 
+    {
+        Serial.print("Connecting to ");
+        Serial.print(ssid);
+        Serial.println("...");
+        WiFi.begin(ssid, pass);
+       
+        if (WiFi.waitForConnectResult() != WL_CONNECTED)
+            return;
+        Serial.println("WiFi connected");
+    }
 
-    if (WiFi.waitForConnectResult() != WL_CONNECTED)
-      return;
-    Serial.println("WiFi connected");
-  }
-
-  // подключаемся к MQTT серверу
-  if (WiFi.status() == WL_CONNECTED) {
-    if (!client.connected()) {
-      Serial.println("Connecting to MQTT server");
-      if (client.connect(MQTT::Connect("arduinoClient2")
+    // подключаемся к MQTT серверу
+    if (WiFi.status() == WL_CONNECTED) {
+        if (!client.connected()) {
+            Serial.println("Connecting to MQTT server");
+            if (client.connect(MQTT::Connect("arduinoClient2")
                          .set_auth(mqtt_user, mqtt_pass))) {
-        Serial.println("Connected to MQTT server");
-        client.set_callback(callback);
-        client.subscribe("test/blink"); // подписывааемся по топик с данными для светодиода
-      } else {
-        Serial.println("Could not connect to MQTT server");
-      }
-    }
+                Serial.println("Connected to MQTT server");
+                client.set_callback(callback);
+                client.subscribe("test/blink");    // подписывааемся по топик с данными для светодиода
+            } 
+            else {
+                Serial.println("Could not connect to MQTT server");
+            }
+        }
 
-    if (client.connected()) {
-        client.loop();
+        if (client.connected()) {
+            client.loop();
+        }
+        cell1.updateLoops();
     }
-    cell1.updateLoops();
-
-  }
-} // конец основного цикла
+} 
